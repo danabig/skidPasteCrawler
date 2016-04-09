418d941be7b15d4b0ae1eb0be29fd8dfd540ff70
danabig <dan.abig@gmail.com>
2016-04-09 20:20:15 +0300

    ui change

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 import os, json
 from MyServer import MyServer
-from flask import Flask, Response, request
+from flask import Flask, Response
 
 app = Flask(__name__, static_url_path='', static_folder='public')
 app.add_url_rule('/', 'root', lambda: app.send_static_file('index.html'))
